a1a6f5ef8de1235032a7a672d350d26ab660350c
yhenifer <97737141+yhenifer@users.noreply.github.com>
2022-06-06 15:10:17 +0200

    Level 2 Challenge - respond from url with _parameter_!

--- server.js
+++ server.js
@@ -11,20 +11,26 @@ const quotes = require("./quotes.json");
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
 
-//   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
+//   /quotes            - Should return all quotes (json)
 app.get("/quotes", function (request, response) {
   response.send(quotes);
 });
-
+//   /quotes/random     - Should return ONE quote (json)
 app.get("/quotes/random", function (request, response) {
   response.send(pickFromArray(quotes));
+});
+
+
+app.get('/quotes/search', (request, response) => {
+  const term = request.query.term.toLowerCase()  
+  const search = quotes.filter(quote => quote.quote.toLowerCase().includes(term) || quote.author.toLowerCase().includes(term))
+  response.send(search)
 });
 
 //...END OF YOUR CODE
